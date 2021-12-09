2fba7f5740b84484b3bc5cb9b75fc3d5a667ca66
j[E]sMaM[o]M <jame3032002@hotmail.com>
2021-12-09 11:29:17 +0700

    Example 2-1: Work Queues (ใช้ prefetch)

--- consumer/server.js
+++ consumer/server.js
@@ -14,13 +14,15 @@ amqp.connect('amqp://kajame:111111@localhost:5672', function (error0, connection
 
     // This makes sure the queue is declared before attempting to consume from it
     channel.assertQueue(queue, { durable: true })
+    channel.prefetch(1)
     channel.consume(queue, function (msg) {
       const secs = Math.floor(Math.random() * 10) // msg.content.toString().split('-')[1]
       console.log(' [x] Received %s %d', msg.content.toString(), secs)
 
       setTimeout(function () {
         console.log(' [x] Done')
+        channel.ack(msg)
       }, secs * 1000)
-    }, { noAck: true })
+    }, { noAck: false })
   })
 })
